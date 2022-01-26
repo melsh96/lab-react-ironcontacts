e3c50e4d5c220b37787b294843c0ed22e1fa5e5c
Melodie Shahrvari <mshahrvari@gmail.com>
2022-01-26 09:51:07 +0100

    Iterationn 1 - 5

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ function App() {
   const [contacts, setContacts] = useState(personnes.slice(0, 5));
 
   function randomContact() {
-    const index = Math.floor(Math.random() * (personnes.length));
-    let newContact = personnes[index];
-    // 1) copy state 
-    let copy = [...contacts];
+    const currentIds = contacts.map((a) => a.id);
+    const pseudoRandom = personnes.filter((c) => {
+      if (!currentIds.includes(c.id)) {
+        return c;
+      }
+    });
+
+    const index = Math.floor(Math.random() * pseudoRandom.length);
+    // let newContact = personnes[index];
+    // 1) copy state
+    let copy = [...contacts, pseudoRandom[index]];
 
     // 2) New contact to copy
-    copy.push(newContact);
+    copy.push(copy);
 
     // 3) give setContacts the copy
     setContacts(copy);
-
   }
 
   function sortName() {
@@ -26,7 +32,7 @@ function App() {
     });
     // give setContacts the copy
     setContacts([...sortedNames]);
-  };
+  }
 
   function sortPopularity() {
     const sortedList = contacts.sort((a, b) => {
@@ -34,58 +40,61 @@ function App() {
     });
     // give setContacts the copy
     setContacts([...sortedList]);
-  };
+  }
 
   function deleteContact(id) {
-    const filteredContacts = contacts.filter((personnes) => {
-      return personnes.id !== id;
-    });
-    setContacts([...filteredContacts]);
-  };
+    // const filteredContacts = contacts.filter((personnes) => {
+    //   return personnes.id !== id;
+    // });
+    // setContacts([...filteredContacts]);
+    const copy = contacts.filter((c) => c.id !== id);
+    setContacts(copy);
+  }
 
-  return <div className="App">
-        <h1>IronContacts</h1>
-        <button onClick={randomContact}>Add Random Contact</button>
-        <button onClick={() => sortName()}>Sort by name</button>
-        <button onClick={() => sortPopularity()}>Sort by popularity</button>
-    <div className="table-one">
-      <table >
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-            <th>Won Oscar</th>
-            <th>Won Emmy</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-
-          {contacts.map((element) => (
-            <tr key={element.id}>
-                  <td>
-                    <img
-                      src={element.pictureUrl}
-                      alt={element.name}
-                      style={{ width: "100px" }}
-                    />
-                  </td>
-                  <td>{element.name}</td>
-                  <td>{element.popularity}</td>
-                  <td>{element.wonOscar ? "🏆" : ""}</td>
-                  <td>{element.wonEmmy ? "🌟" : ""}</td>
-                  <td><button onClick={() => deleteContact(contacts.id)}>Delete</button></td>
-                </tr>
-                
-          ))}
-          
-        </tbody>
-      </table>
+  return (
+    <div className="App">
+      <h1>IronContacts</h1>
+      <button onClick={randomContact}>Add Random Contact</button>
+      <button onClick={() => sortName()}>Sort by name</button>
+      <button onClick={() => sortPopularity()}>Sort by popularity</button>
+      <div className="table-one">
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Won Oscar</th>
+              <th>Won Emmy</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contacts.map((element) => (
+              <tr key={element.id}>
+                <td>
+                  <img
+                    src={element.pictureUrl}
+                    alt={element.name}
+                    style={{ width: "100px" }}
+                  />
+                </td>
+                <td>{element.name}</td>
+                <td>{element.popularity}</td>
+                <td>{element.wonOscar ? "🏆" : ""}</td>
+                <td>{element.wonEmmy ? "🌟" : ""}</td>
+                <td>
+                  <button onClick={() => deleteContact(contacts.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
-
-
